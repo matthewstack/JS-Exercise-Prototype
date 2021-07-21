27276ae18ac3462a9f6f123c1799e1ef76498c1f
Matthew Stack <matthewstack@Matthews-MacBook-Pro-4.local>
2021-07-21 15:05:28 -0400

    Task 1

--- index.js
+++ index.js
@@ -39,13 +39,36 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = []
+}
+Person.prototype.eat = function(edible){
+    if(this.stomach.length < 10){
+      this.stomach.push(edible);
+    }
+}
+Person.prototype.poop = function() {
+  this.stomach = [];
 }
 
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
+}
 
+const matt = new Person('Matt', 34)
 
-
+matt.eat('🍖');
+matt.eat('🍋');
+matt.eat('🍎');
+matt.eat('🍟');
+matt.eat('🥥');
+matt.eat('🍘');
+matt.eat('🍢');
+matt.eat('🍧');
+matt.eat('🍈');
+matt.eat('🍇');
 
 
 
